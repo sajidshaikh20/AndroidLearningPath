5481967e97a3e90267d87d8849f5b8efe9130b11
sajid_shaikh <sajid.shaikh@brainvire.com>
2024-04-16 10:08:03 +0530

    userForm viewmodel

--- app/src/main/java/com/base/hilt/ui/userForm/viewmodel/UserFormViewModel.kt
+++ app/src/main/java/com/base/hilt/ui/userForm/viewmodel/UserFormViewModel.kt
@@ -1,10 +1,8 @@
 package com.base.hilt.ui.userForm.viewmodel
 
 import android.util.Log
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.base.hilt.base.ViewModelBase
-import com.base.hilt.di.UserRepository
 import com.base.hilt.ui.userForm.model.UserData
 import com.base.hilt.ui.userForm.repository.UserFormRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
